045d3f72235786289dd80ba93d5f5f0d7f8c20d1
fathallah <60329067+fathallahma@users.noreply.github.com>
2024-06-28 14:28:37 +0200

    Update DefaultSuiteTest.java

--- DefaultSuiteTest.java
+++ DefaultSuiteTest.java
@@ -22,6 +22,7 @@ import org.openqa.selenium.Keys;
 import java.util.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+
 public class DefaultSuiteTest {
   private WebDriver driver;
   private Map<String, Object> vars;
